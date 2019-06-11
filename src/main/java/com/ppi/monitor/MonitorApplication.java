181bf70a0a34225e690d15a0b0979b3ffe8934f3
paulaj <paujaramillos@gmail.com>
2019-06-11 12:33:17 -0500

    Update 11/06/2019

--- src/main/java/com/ppi/monitor/MonitorApplication.java
+++ src/main/java/com/ppi/monitor/MonitorApplication.java
@@ -6,8 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MonitorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MonitorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+
+//        try {
+//            FirebaseOptions options = new FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource("/firebase-authentication.json").getInputStream()))
+//                    .setDatabaseUrl(FB_BASE_URL)
+//                    .build();
+//            FirebaseApp.initializeApp(options);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
 
 }
---
+++ src/main/java/com/ppi/monitor/util/FirebaseConections.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ppi.monitor.util;
+
+
+
+
+
+
+
+/**
+ *
+ * @author 57314
+ */
+public class FirebaseConections {
+
+
+}
